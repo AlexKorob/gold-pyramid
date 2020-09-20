9db3cbfee97b584d8b4c84247b3e490e9c842670
developer <developer@gmail.com>
2020-09-20 14:19:57 +0300

    Updates

---
+++ precache-manifest.f737c88184bc929ac77577f582c8359e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23711ab008dd220b025127118f93f993",
+    "url": "/gold-pyramid/index.html"
+  },
+  {
+    "revision": "28546e04ada818d13773",
+    "url": "/gold-pyramid/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ad0b437b65e5c88d6e98",
+    "url": "/gold-pyramid/static/js/2.be2d66db.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gold-pyramid/static/js/2.be2d66db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28546e04ada818d13773",
+    "url": "/gold-pyramid/static/js/main.118d27d7.chunk.js"
+  },
+  {
+    "revision": "8153897027547af32a86",
+    "url": "/gold-pyramid/static/js/runtime-main.6e6bd23d.js"
+  }
+]);
